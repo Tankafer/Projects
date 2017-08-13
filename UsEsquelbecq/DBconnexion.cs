76119392434ca90bd9e5f92ac2077b1999fbe8af
Afer <Afer@LAPTOP-AFER>
2017-08-13 17:12:24 +0200

    1st Commit

---
+++ UsEsquelbecq/DBconnexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UsEsquelbecq
+{
+    class DBconnexion
+    {
+        public DBconnexion()
+        {
+            string connexionString = "server=localhost;database=dbusesquelbecq;uid=root;pwd=;SslMode=none";
+            MySqlConnection cnn = new MySqlConnection(connexionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+
+        }
+    }
+}
---
+++ UsEsquelbecq/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace UsEsquelbecq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DBconnexion xmlFile = new DBconnexion();
+        }
+    }
+}
